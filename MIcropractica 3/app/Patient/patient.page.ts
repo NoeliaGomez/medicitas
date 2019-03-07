f6f905b11b19c0104c2f3acd2ada4b1b03abccad
pineox <47446384+pineox@users.noreply.github.com>
2019-03-08 00:03:56 +0100

    Add files via upload

---
+++ MIcropractica 3/app/Patient/patient.page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-patient',
+  templateUrl: 'patient.page.html',
+  styleUrls: ['patient.page.scss'],
+})
+export class PatientPage {
+
+}
---
+++ MIcropractica 3/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+
+import { Platform } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+import { HomePage } from './home/home.page';
+import { PatientPage } from './Patient/patient.page';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html'
+})
+export class AppComponent {
+
+  constructor(
+    private platform: Platform,
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar
+  ) {
+    this.initializeApp();
+    this.pages = [
+      { title: 'Home', component: HomePage },
+      { title: 'Patient', component: PatientPage },
+    ];
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+    });
+  }
+}
